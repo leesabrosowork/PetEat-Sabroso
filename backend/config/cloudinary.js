b255f1c7808a08f955ff826a6f917406461172f4
Robert Angelo Herrera <ohbeeboi@gmail.com>
2025-06-20 00:58:50 +0800

    fixed dashboards with bugs: duplicate accounts, splash screen of vet clinic after account creation, emr not loading, business permit image not loading

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'prld-capstone',
-  api_key: process.env.CLOUDINARY_API_KEY || '471123992644262',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'Ry_Kc0Xj_Wd_Wd_Wd_Wd_Wd_Wd_Wd'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dyscnbdva',
+  api_key: process.env.CLOUDINARY_API_KEY || '641545256255234',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'RMnTNL5C5lOvik2skY1yebRanPs'
 });
 
 module.exports = cloudinary; 
